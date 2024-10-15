5e42ccca7ef743a56303d01ceb98bd7fda591ffa
unknown <dilshodadilbekov456@gmail.com>
2024-10-16 01:32:55 +0600

    login age is created

---
+++ src/components/pages/AuthPage/SignIn.tsx
@@ -0,0 +1,49 @@
+"use client";
+import FastSignIn from "@/components/ui/FastSignIn";
+import Link from "next/link";
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+interface IInputValue {
+  email: string;
+}
+const SignIn = () => {
+  const { register, handleSubmit } = useForm<IInputValue>();
+  const onSubmit: SubmitHandler<IInputValue> = (IInputValue) => {};
+  return (
+    <section>
+      <div className="container">
+        <div className="flex flex-col px-5 py-5 rounded-xl bg-blue-800/35 gap-y-5">
+          <h1 className="text-white font-bold text-3xl">Login</h1>
+          <form className="flex flex-col gap-y-4">
+            <input
+              type="email"
+              className="bg-white text-neutral-700 placeholder:text-neutral-400 focus:outline-none rounded-md px-3"
+              placeholder="email"
+              {...register("email", { required: true })}
+            />
+            <button
+              type="submit"
+              className="bg-white text-neutral-900 font-bold px-5 py-2 rounded-md"
+            >
+              Login
+            </button>
+          </form>
+          <FastSignIn />
+          <div className="flex items-center py-4 border-t border-neutral-400 gap-x-2">
+            <p className="text-neutral-400 font-normal ">
+              If you have not account
+            </p>
+            <Link
+              href="/auth/sign_up"
+              className="text-white font-normal hover:underline hover:opacity-75 transition"
+            >
+              Sign-up free
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignIn;
---
+++ src/components/ui/FastSignIn.tsx
@@ -0,0 +1,22 @@
+import { signIn } from "next-auth/react";
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+const FastSignIn = () => {
+  return (
+    <div className="w-full flex items-center gap-x-4">
+      <FaGithub
+        onClick={() => signIn("github")}
+        size={35}
+        className="text-black hover:opacity-75 transition"
+      />
+      <FcGoogle
+        size={35}
+        onClick={() => signIn("google")}
+        className=" hover:opacity-75 transition"
+      />
+    </div>
+  );
+};
+
+export default FastSignIn;
